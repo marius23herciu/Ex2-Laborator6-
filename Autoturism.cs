5dfac45fe339803181a311cbad72953d363df812
marius.herciu23 <marius.herciu23@gmail.com>
2022-06-11 16:20:28 +0300

    Add project files.

---
+++ Autoturism.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex2_Laborator6_
+{
+    class Autoturism
+    {
+        private string marca;
+        private string numarInmatriculare;
+        private int capacitateCilindrica;
+
+        /// <summary>
+        /// Modeleaza un autoturism
+        /// </summary>
+        /// <param name="marca">Marca autoturismului</param>
+        /// <param name="numarInmatriculare">Nuarul de inmatriculare</param>
+        /// <param name="capacitateCilindrica">Capacitatea cilindrica</param>
+        public Autoturism(string marca, string numarInmatriculare, int capacitateCilindrica)
+        {
+            this.marca = marca;
+            this.numarInmatriculare = numarInmatriculare;
+            this.capacitateCilindrica = capacitateCilindrica;
+        }
+
+        /// <summary>
+        /// Formeaza un string cu descrierea autoturismului
+        /// </summary>
+        /// <returns></returns>
+        public string GetDescription()
+        {
+            string descriereAutoturism = $"Autoturismul {marca} are numarul de inmatriculare {numarInmatriculare} si capacitatea cilindrica {capacitateCilindrica}";
+            return descriereAutoturism;
+        }
+
+        /// <summary>
+        /// Tipareste caracteristicile autoturismului
+        /// </summary>
+        public void Tipareste()
+        {
+            Console.WriteLine(GetDescription());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex2_Laborator6_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Autoturism dacia = new Autoturism("Dacia", "BC 01 BCB", 1400);
+            Autoturism fiat = new Autoturism("Fiat", "CJ 01 CJC", 1500);
+
+            dacia.Tipareste();
+            fiat.Tipareste();
+        }
+    }
+}
